a7c3062eff85e95b074f4988e548bef9d16222b5
AlexandraGale <77981904+AlexandraGale@users.noreply.github.com>
2024-03-26 22:20:14 -0400

    updateing boilerplate

--- client.c
+++ client.c
@@ -11,17 +11,25 @@ int main()
         { int err;
           char my_buf[1024] ;
           char *msg = "I am your main socket squeeze" ;
-
-       
+          FILE *ptr ; 
+          long fileSize;
+          long recv_size;
 
          int my_sock = socket(AF_INET, SOCK_STREAM, 0) ;
          sa.sin_family   = AF_INET ;
-         sa.sin_port     = htons(8003) ;
-         inet_pton(AF_INET, "xxx.xx.xxx.x" , &(sa.sin_addr)) ; //fill in IP address of server
+         sa.sin_port     = htons(8007) ;
+         inet_pton(AF_INET, "127.0.0.1" , &(sa.sin_addr)) ; //fill in IP address of server //
          printf("BEFORE CONNECT\n") ;
          int x = connect(my_sock, (struct sockaddr *) &sa, sizeof(sa)) ;
          if(x < 0)
 		    perror("Connect") ;
+
+        recv_size = recv(my_sock , &fileSize , sizeof(long), 0) ;
+        long Fsize = htonl(fileSize) ;  //use Fsize
+
+        long numChuncks = Fsize / 1024;
+        long lastChunck = Fsize % 1024; 
+
 
          printf ("Sending Message to Server!\n") ;
          send (my_sock, msg, 32,  0) ;
--- server.c
+++ server.c
@@ -12,19 +12,28 @@ int main(int argc , char *argv[])
     socklen_t c ;
     struct sockaddr_in server , client, my_addr;
     char client_message[20000];
-
+    FILE *ptr;
     socket_desc = socket(AF_INET , SOCK_STREAM , 0);
     client_sock = socket(AF_INET , SOCK_STREAM , 0);
     
     server.sin_family = AF_INET;
-    server.sin_port = htons(8003);
-    inet_pton (AF_INET, "xxx.xx.xxx.x", &(server.sin_addr)) ; //fill in server ip address
+    server.sin_port = htons(8007);
+    inet_pton (AF_INET, "127.0.0.1", &(server.sin_addr)) ; //fill in server ip address // loopback: 127.0.0.1
     if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
     {
         perror("bind failed. Error");
         return 1;
     }
-   printf("Socket is bound! Waiting for connections!\n") ;
+   printf("Socket is bound! Waiting for connections!\n");
+
+    ptr =fopen("BM(1).txt", "W");
+    fseek(ptr, 0, SEEK_END);//seek to eof
+    long fileSize = ftell(ptr);//get current value of file pointer location
+    fseek(ptr, 0, SEEK_SET);//back to start of file 
+
+    long fsize = htonl(fileSize) ;
+    send_size = send(client_sock, &fsize, sizeof(fsize),0);
+    
 
  listen(socket_desc , 1); 
  client_sock = accept(socket_desc, (struct sockaddr *)&client, &c);    
